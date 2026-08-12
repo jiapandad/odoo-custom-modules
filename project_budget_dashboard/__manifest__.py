cb420ba328a39f5c9d3ab48ede808684e2dce7e8
jiapandad <106260083+jiapandad@users.noreply.github.com>
2026-08-12 11:08:50 +0800

    Add project_budget_dashboard/__manifest__.py

---
+++ project_budget_dashboard/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "项目预算仪表板",
+    "version": "19.0.1.0.0",
+    "depends": [
+        "project",
+        "purchase",
+        "analytic"
+    ],
+    "data": [
+        "views/project_views.xml"
+    ],
+    "installable": True
+}
