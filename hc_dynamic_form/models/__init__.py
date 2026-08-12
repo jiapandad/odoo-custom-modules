cd823a53c89f87b526e165b85fbdd600b4f1fcec
jiapandad <106260083+jiapandad@users.noreply.github.com>
2026-08-12 11:12:51 +0800

    Add hc_dynamic_form/models/__init__.py

---
+++ hc_dynamic_form/models/__init__.py
@@ -0,0 +1,6 @@
+from . import form_template
+from . import form_template_field
+from . import form_instance
+from . import form_instance_field
+from . import setup_wizard
+from . import approval_patch
