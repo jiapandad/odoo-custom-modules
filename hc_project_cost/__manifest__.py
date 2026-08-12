d8792bd69d6c891f4c93d2115b5d6067292e6cd9
jiapandad <106260083+jiapandad@users.noreply.github.com>
2026-08-12 11:12:55 +0800

    Add hc_project_cost/__manifest__.py

---
+++ hc_project_cost/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "HC Project Cost",
+    "version": "19.0.1.0.0",
+    "category": "Project",
+    "summary": "项目总花费计算（PO + 工时）",
+    "author": "HC",
+    "depends": ["project", "purchase"],
+    "data": ["views/project_project_view.xml"],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+    "license": "LGPL-3",
+}
