06082aeafb5d80c833cb7248fb59b459c316301a
jiapandad <106260083+jiapandad@users.noreply.github.com>
2026-08-12 11:13:25 +0800

    Add project_purchase_requisition/__manifest__.py

---
+++ project_purchase_requisition/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Project Purchase Requisition Bridge",
+    "version": "19.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": [
+        "hr_expense","purchase", "project", "analytic"],
+    "author": "Custom",
+    "category": "Project",
+    "data": [
+        "views/purchase_views.xml",
+        "views/expense_views.xml"
+    ],
+    "installable": True,
+}
